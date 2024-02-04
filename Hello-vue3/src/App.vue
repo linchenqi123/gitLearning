94ae84a3e78f8fd8f591dddb03160e9b63bfddc4
linchenqi123 <891679600@qq.com>
2024-02-04 20:50:58 +0800

    Add Vue 3 project files and configurations

---
+++ Hello-vue3/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+ <Person/>
+</template>
+
+<script setup lang="ts">
+import Person from './components/Person.vue'
+</script>
+
+<style>
+
+</style>
---
+++ Hello-vue3/src/components/Person.vue
@@ -0,0 +1,11 @@
+<template>
+  你好啊
+</template>
+
+<script lang="ts" setup name="Person">
+
+</script>
+
+<style>
+
+</style>
